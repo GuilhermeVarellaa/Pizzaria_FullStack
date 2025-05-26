511fec9825f6c1a2d24c23ae07264a7fc905bb90
Guilherme Varella <guilhermeraul1@hotmail.com>
2025-05-25 22:35:17 -0300

    começo da implementação de seguranca com JWT

---
+++ pizzaria/src/main/java/com/pizzaria/pizzaria/service/CustomUserDetailsService.java
@@ -0,0 +1,9 @@
+package com.pizzaria.pizzaria.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+}
